3c1b638186ec0737bbb286d08ef6efcbc8376d51
jinn <booboo080yamaya@gmail.com>
2020-09-08 15:00:28 +0900

    DotEnvでトークンを使えるようにする

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'dart:ffi';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:mobile_course_flutter/main_app.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async{
+  await DotEnv().load('.env');
   runApp(MainApp());
 }
--- lib/main_app.dart
+++ lib/main_app.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final token = DotEnv().env['API_TOKEN'];
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text("hello"),
+          child: Text(token),
         ),
       ),
     );
