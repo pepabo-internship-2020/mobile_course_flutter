5d11918ceeae97d46cb7379074255cb983a810f6
shuent <shunshun.43@gmail.com>
2020-09-09 14:48:02 +0900

    create models

--- lib/model/product.dart
+++ lib/model/product.dart
@@ -1,1 +1,35 @@
-class Product {}
+import 'dart:convert';
+
+class Product {
+  final int id;
+  final String title;
+  final String sampleImageUrl;
+  final String url;
+
+  Product({this.id, this.title, this.sampleImageUrl, this.url});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'sampleImageUrl': sampleImageUrl,
+      'url': url,
+    };
+  }
+
+  factory Product.fromMap(Map<String, dynamic> map) {
+    if (map == null) return null;
+
+    return Product(
+      id: map['id'],
+      title: map['title'],
+      sampleImageUrl: map['sampleImageUrl'],
+      url: map['url'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Product.fromJson(String source) =>
+      Product.fromMap(json.decode(source));
+}
--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,1 +1,34 @@
-class User {}
+import 'dart:convert';
+
+class User {
+  final int id;
+  final String name;
+  final String displayName;
+  final String avatarUrl;
+
+  User({this.id, this.name, this.displayName, this.avatarUrl});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'displayName': displayName,
+      'avatarUrl': avatarUrl,
+    };
+  }
+
+  factory User.fromMap(Map<String, dynamic> map) {
+    if (map == null) return null;
+
+    return User(
+      id: map['id'],
+      name: map['name'],
+      displayName: map['displayName'],
+      avatarUrl: map['avatarUrl'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory User.fromJson(String source) => User.fromMap(json.decode(source));
+}
