1427c5caac5c61576ae0296d876dde613d7f16f9
shuent <shunshun.43@gmail.com>
2020-09-10 12:12:37 +0900

    pageレイアウト整えた

--- lib/page/user_detail.dart
+++ lib/page/user_detail.dart
@@ -14,51 +14,72 @@ class UserDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var userStore = context.watch<UserStore>();
+    final userStore = context.watch<UserStore>();
 
     if (userStore.user == null) {
-      return Center(
-        child: CircularProgressIndicator(),
+      return Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
       );
     }
     return Scaffold(
-      appBar: AppBar(
-        title: Text(userStore.user.name),
-      ),
       body: CustomScrollView(
         slivers: [
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (context, _) => Column(
-                children: [
-                  Row(
+          SliverAppBar(
+            floating: true,
+            pinned: true,
+            snap: true,
+            expandedHeight: 150.0,
+            flexibleSpace: FlexibleSpaceBar(
+              title: userStore.user.displayName != null
+                  ? Text(userStore.user.displayName)
+                  : Text(userStore.user.name),
+              background: SafeArea(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
                     children: [
                       CircleAvatar(
+                        radius: 40,
                         backgroundImage: NetworkImage(userStore.user.avatarUrl),
-                      ),
-                      Column(
-                        children: [
-                          Text(userStore.user.name),
-                          if (userStore.user.displayName != null)
-                            Text(userStore.user.displayName),
-                        ],
                       ),
                     ],
                   ),
-                ],
+                ),
               ),
-              childCount: 1,
             ),
           ),
           SliverGrid(
-            gridDelegate:
-                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 0.8,
+            ),
             delegate: SliverChildBuilderDelegate(
                 (context, index) => GestureDetector(
                     onTap: () =>
                         _launchURL(userStore.products[index].sampleUrl),
-                    child: Image.network(
-                        userStore.products[index].sampleImageUrl)),
+                    child: Container(
+                      color: Colors.white,
+                      child: Column(
+                        children: [
+                          Image.network(
+                            userStore.products[index].sampleImageUrl,
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 8.0),
+                            child: Text(
+                              userStore.products[index].title,
+                              textAlign: TextAlign.center,
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                              style: Theme.of(context).textTheme.bodyText2,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )),
                 childCount: userStore.products.length),
           )
         ],
